b821a90fe6e70aad50851d066918dca683d3fe69
Biel Matamala <Biel.matamala.09@ipsi.cat>
2026-03-13 12:32:14 +0100

    fsdfsdf

--- Dino_Game/main.py
+++ Dino_Game/main.py
@@ -29,12 +29,53 @@ clock = pygame.time.Clock()
 font = pygame.font.Font(None, 36)
 
 # Variables Globals 
-RUNNING = [PG.image.load(os.path.join(r"dinogames\dino-master\Assets\Dino", r"DinoRun1.png")),
-           PG.image.load(os.path.join(r"dinogames\dino-master\Assets\Dino", r"DinoRun2.png"))]
+RUNNING = [PG.image.load(os.path.join("Assets/Dino", r"DinoRun1.png")),
+           PG.image.load(os.path.join("Assets/Dino", r"DinoRun2.png"))]
+
+JUMPING = PG.image.load(os.path.join("Assets/Dino", r"DinoJump.png"))
+
+DOWNING = [PG.image.load(os.path.join("Assets/Dino", r"DinoDuck1.png")),
+           PG.image.load(os.path.join("Assets/Dino", r"DinoDuck2.png"))]
+
+SMALL_CACTUS = [pygame.image.load(os.path.join("Assets/Cactus", "SmallCactus1.png")),
+                pygame.image.load(os.path.join("Assets/Cactus", "SmallCactus2.png")),
+                pygame.image.load(os.path.join("Assets/Cactus", "SmallCactus3.png"))]
+
+LARGE_CACTUS = [pygame.image.load(os.path.join("Assets/Cactus", "LargeCactus1.png")),
+                pygame.image.load(os.path.join("Assets/Cactus", "LargeCactus2.png")),
+                pygame.image.load(os.path.join("Assets/Cactus", "LargeCactus3.png"))]
+
+BIRD = [pygame.image.load(os.path.join("Assets/Bird", "Bird1.png")),
+        pygame.image.load(os.path.join("Assets/Bird", "Bird2.png"))]
+
+CLOUD = pygame.image.load(os.path.join("Assets/Other", "Cloud.png"))
+
+BG = pygame.image.load(os.path.join("Assets/Other", "Track.png"))
+
 
 #Classes
-class Dino(pygame.sprite.Sprite):
+class Dinosarure:
+    X_POS = 80
+    Y_POS = 310
+    Y_POS_DUCK = 340
+    JUMP_VEL = 8.5
+
     def __init__(self):
-        self.duck_img = DUCKING
+        self.down = DOWNING
         self.run_img = RUNNING
-        self.jump_img = JUMPING
+        self.jump = JUMPING
+        self.step_index = 0
+        self.image = self.run_img[0]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = self.X_POS
+        self.dino_rect.y = self.Y_POS
+    
+    def avançar(self):
+        self.run = self.run_img[self.step_index // 5]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = self.X_POS
+        self.dino_rect.y = self.Y_POS
+        self.step_index += 1
+
+
+
